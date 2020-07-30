9bb67bd0fed9e9f1e32e739f639b288467dd0a4f
Tuan Dang <37445602+danglive@users.noreply.github.com>
2020-07-30 18:49:32 +0200

    Update detection_utils.py

--- detectron2/data/detection_utils.py
+++ detectron2/data/detection_utils.py
@@ -581,12 +581,11 @@ def build_augmentation(cfg, is_train):
         
         contrast_min, contrast_max     = cfg.INPUT.CONTRAST.RANGE
         brightness_min, brightness_max = cfg.INPUT.BRIGHTNESS.RANGE 
-        saturation_min, saturation_max = cfg.INPUT.SATURATION.RANGE 
+
         
         augmentation.append(T.RandomFlip())
         augmentation.append(T.RandomBrightness(contrast_min, contrast_max))
-        augmentation.append(T.RandomContrast(brightness_min, brightness_max))
-        augmentation.append(T.RandomSaturation(saturation_min, saturation_max))     
+        augmentation.append(T.RandomContrast(brightness_min, brightness_max))    
     return augmentation
 
 
